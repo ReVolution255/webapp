a5dcb3aa653508bc3badc395278c214ed1b86355
ReVolution255 <grivoos@yandex.com>
2015-07-21 20:03:18 +0300

    Errors fixed

--- sql_init.sql
+++ sql_init.sql
@@ -5,7 +5,7 @@ name varchar
 CREATE TABLE groups (
 id bigserial PRIMARY KEY,
 name varchar,
-parent_id REFERENCES group (id)
+parent_id bigint REFERENCES groups(id)
 );
 CREATE TABLE roles (
 id bigserial PRIMARY KEY,
@@ -17,16 +17,16 @@ name varchar
 );
 CREATE TABLE user_groups (
 id bigserial PRIMARY KEY,
-user_id bigserial REFERENCES users (id),
-group_id bigserial REFERENCES groups (id)
+user_id bigserial REFERENCES users(id),
+group_id bigserial REFERENCES groups(id)
 );
 CREATE TABLE role_permissions (
 id bigserial PRIMARY KEY,
-permission_id bigserial REFERENCES permissions (id),
-role_id bigserial REFERENCES roles (id)
+permission_id bigserial REFERENCES permissions(id),
+role_id bigserial REFERENCES roles(id)
 );
 CREATE TABLE user_roles (
 id bigserial PRIMARY KEY,
-role_id bigserial REFERENCES roles (id),
-user_id bigserial REFERENCES users (id)
+role_id bigserial REFERENCES roles(id),
+user_id bigserial REFERENCES users(id)
 );
