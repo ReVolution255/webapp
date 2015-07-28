0c5d97da40b4afbe2c06c412d2e136784f70a01e
ReVolution255 <grivoos@yandex.com>
2015-07-28 18:36:16 +0400

    AJAX realized.

---
+++ src/main/webapp/scripts/common.js
@@ -0,0 +1,103 @@
+var req;
+var isIE;
+
+function init() {
+
+}
+
+function deleteUser(){
+    var elem = document.getElementById('idfield_deleteform');
+    var id = elem.value;
+    var url = "?action=delete&delete=" + id;
+    req = createReq();
+    req.open("GET", url, true);
+    req.send();
+    req.onreadystatechange = updateTable;
+    elem.value = "";
+}
+
+function createUser(){
+    var elem = document.getElementById('namefield_createform');
+    var name = elem.value;
+    var url = "?action=create&name=" + name;
+    req = createReq();
+    req.open("GET", url, true);
+    req.send();
+    req.onreadystatechange = updateTable;
+    elem.value = "";
+}
+
+function editUser(){
+    var elem1 = document.getElementById('idfield_editform');
+    var id = elem1.value;
+    var elem2 = document.getElementById('namefield_editform');
+    var name = elem2.value;
+    var url = "?action=edit&id=" + id + "&name=" + name;
+    req = createReq();
+    req.open("GET", url, true);
+    req.send();
+    req.onreadystatechange = updateTable;
+    elem1.value = "";
+    elem2.value = "";
+}
+
+function createReq(){
+    if (window.XMLHttpRequest) {
+        if (navigator.userAgent.indexOf('MSIE') != -1) {
+            isIE = true;
+        }
+        return new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        isIE = true;
+        return new ActiveXObject("Microsoft.XMLHTTP");
+    }
+}
+
+function updateTable(){
+    var users = $.parseJSON(req.responseText);
+    var table = document.getElementById("users");
+    table.innerHTML = "";
+    $.each(users, function(number, user){
+        //Links 'about' page
+        var a_id = document.createElement('a');
+        var a_name = document.createElement('a');
+        a_id.setAttribute('href', 'about-user?id=' + user.id);
+        a_name.setAttribute('href', 'about-user?id=' + user.id);
+        //Table
+        var tr = document.createElement('tr');
+        var td_id = document.createElement('td');
+        var td_name = document.createElement('td');
+
+        var td_edit = document.createElement('td');
+        var temp_form = document.createElement('form');
+        temp_form.setAttribute('action', 'edit-user.jsp');
+        var temp_button = document.createElement('button');
+        temp_button.setAttribute('name', 'edit');
+        temp_button.setAttribute('type', 'submit');
+        temp_button.setAttribute('value', user.id);
+        temp_button.innerHTML = "Edit";
+        temp_form.appendChild(temp_button);
+        td_edit.appendChild(temp_form);
+
+        var td_delete = document.createElement('td');
+        temp_form = document.createElement('form');
+        temp_form.setAttribute('action', 'delete-user.jsp');
+        temp_button = document.createElement('button');
+        temp_button.setAttribute('name', 'delete');
+        temp_button.setAttribute('type', 'submit');
+        temp_button.setAttribute('value', user.id);
+        temp_button.innerHTML = "Delete";
+        temp_form.appendChild(temp_button);
+        td_delete.appendChild(temp_form);
+
+        a_id.appendChild(document.createTextNode(user.id));
+        a_name.appendChild(document.createTextNode(user.name));
+        td_id.appendChild(a_id);
+        td_name.appendChild(a_name);
+        tr.appendChild(td_id);
+        tr.appendChild(td_name);
+        tr.appendChild(td_edit);
+        tr.appendChild(td_delete);
+        table.appendChild(tr);
+    });
+}
--- src/main/webapp/users.jsp
+++ src/main/webapp/users.jsp
@@ -5,32 +5,44 @@
 <head>
     <title>Users</title>
 </head>
-<body>
-<form action="add-user.jsp">
-    <button>
-        Add user
-    </button>
-</form>
+<script src="<c:out value="${pageContext.request.contextPath}" />/scripts/common.js"></script>
+<script src="<c:out value="${pageContext.request.contextPath}" />/scripts/jquery-2.1.4.min.js"></script>
+<body onload="init()">
+    <label for="namefield_createform">User name:</label>
+    <input type="text" id="namefield_createform" name="name" placeholder="User name">
+    <input value="Create" onclick="createUser()" type="button">
 <br>
         <c:choose>
             <c:when test="${usersList.size() > 0}">
-                <form action="delete-user.jsp">
-                    <button>
-                        Delete user
-                    </button>
-                </form>
-                <table border=1>
-                    <br>
+                    <label for="idfield_deleteform">User id:</label>
+                    <input type="text" id="idfield_deleteform" name="delete" placeholder="User id">
+                    <input onclick="deleteUser()" type="button" value="Delete">
+                <br>
+                    <label for="idfield_editform">User id:</label>
+                    <input type="text" id="idfield_editform" name="id" placeholder="User id">
+                    <label for="namefield_editform">User name:</label>
+                    <input type="text" id="namefield_editform" name="name" placeholder="New name">
+                    <input onclick="editUser()" type="button" value="Accept">
+                <br>
+                <table id="users" border=1>
             <c:forEach var="user" items="${usersList}">
                 <tr>
-                    <td><a href="about-user?id=<c:out value="${user.id}" />"><c:out value="${user.id}" /></a></td>
-                    <td><a href="about-user?id=<c:out value="${user.id}" />"><c:out value="${user.name}" /></a></td>
-                    <td><form action="edit-user.jsp">
+                    <td>
+                        <a href="about-user?id=<c:out value="${user.id}" />"><c:out value="${user.id}" /></a>
+                    </td>
+                    <td class="username" id="<c:out value="${user.id}" />">
+                        <a href="about-user?id=<c:out value="${user.id}" />"><c:out value="${user.name}" /></a>
+                    </td>
+                    <td>
+                        <form action="edit-user.jsp">
                         <button name="edit" type="submit" value="<c:out value="${user.id}" />">Edit</button>
-                    </form></td>
-                    <td><form action="delete-user">
+                        </form>
+                    </td>
+                    <td>
+                        <form action="delete-user">
                         <button name="delete" type="submit" value="<c:out value="${user.id}" />">Delete</button>
-                    </form></td>
+                        </form>
+                    </td>
                 </tr>
             </c:forEach>
                 </table>
