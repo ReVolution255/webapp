7e83743b1cfeb0e7f8527d7f3a991bf8272607a9
Vladimir Kulishin <grivoos@yandex.com>
2015-08-05 18:55:30 +0300

    DI realized (work).

--- src/main/java/UsersResource.java
+++ src/main/java/UsersResource.java
@@ -4,12 +4,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
 
 import java.util.List;
 
 @Path("/")
+@Component
 @Produces("application/json")
 public class UsersResource {
     private static final Logger logger = LogManager.getLogger();
