490aa1189e1cae18bba6e4ccff313f4c21711f21
Vladimir Kulishin <grivoos@yandex.com>
2015-08-19 20:34:27 +0300

    Cascade removind realized.

--- sql_init.sql
+++ sql_init.sql
@@ -5,7 +5,7 @@ name varchar
 CREATE TABLE groups (
 id bigserial PRIMARY KEY,
 name varchar,
-parent_id bigint REFERENCES groups(id)
+parent_id bigint REFERENCES groups(id) ON DELETE CASCADE
 );
 CREATE TABLE roles (
 id bigserial PRIMARY KEY,
@@ -17,16 +17,16 @@ name varchar
 );
 CREATE TABLE user_groups (
 id bigserial PRIMARY KEY,
-user_id bigserial REFERENCES users(id),
-group_id bigserial REFERENCES groups(id)
+user_id bigserial REFERENCES users(id) ON DELETE CASCADE,
+group_id bigserial REFERENCES groups(id) ON DELETE CASCADE
 );
 CREATE TABLE role_permissions (
 id bigserial PRIMARY KEY,
-permission_id bigserial REFERENCES permissions(id),
-role_id bigserial REFERENCES roles(id)
+permission_id bigserial REFERENCES permissions(id) ON DELETE CASCADE,
+role_id bigserial REFERENCES roles(id) ON DELETE CASCADE
 );
 CREATE TABLE user_roles (
 id bigserial PRIMARY KEY,
-role_id bigserial REFERENCES roles(id),
-user_id bigserial REFERENCES users(id)
+role_id bigserial REFERENCES roles(id) ON DELETE CASCADE,
+user_id bigserial REFERENCES users(id) ON DELETE CASCADE
 );
